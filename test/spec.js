eae6e7ab48f3d9c33a78f90650d637e1bc546404
david hodges <david.hodges@lonelyplanet.com>
2016-07-10 10:36:18 +0100

    initial setup

---
+++ test/spec.js
@@ -0,0 +1,11 @@
+'use strict'
+import 'babel-polyfill'
+
+let expect = require('expect')
+let trillo = require('../dist/trillo')
+
+describe('trillo', () => {
+    return it('knows black from white', () => {
+        return expect('black').toNotEqual('white')
+    })
+})

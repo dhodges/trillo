856719a454e95f7ac03862d0bcfd21dfb3831531
david hodges <david.hodges@lonelyplanet.com>
2016-07-11 21:01:12 +0100

    parseCardActions()

--- src/trillo.js
+++ src/trillo.js
@@ -2,7 +2,27 @@
 
 const config   = require('../config.json'),
       jsonfile = require('jsonfile'),
-      Trello   = require('./trello').Trello
+      Trello   = require('./trello').Trello,
+      _        = require('lodash')
+
+const parseCardActions = (actions) => ({
+  card: {
+    id:   actions[0].data.card.id,
+    name: actions[0].data.card.name
+  },
+  actions: _.map(
+    _.filter(actions, (a) => a.type == 'updateCard'),
+    (action) => ({
+      id:         action.id,
+      date:       action.date,
+      type:       action.type,
+      listAfter:  action.data.listAfter,
+      listBefore: action.data.listBefore,
+      memberCreator: {
+        id:       action.memberCreator.id,
+        fullName: action.memberCreator.fullName
+      },
+    }))})
 
 const run = function() {
   const listId = config.lists[3].id
@@ -16,3 +36,4 @@ const run = function() {
 }
 
 module.exports.run = run
+module.exports.parseCardActions = parseCardActions

ea2e0996a8547ae680bb295fe4adf4d34ef42046
David Hodges <davidh@bermuda.local.net>
2016-07-16 17:30:15 +0100

    spec_helper.js

---
+++ test/spec_helper.js
@@ -0,0 +1,20 @@
+'use strict'
+import 'babel-polyfill'
+
+require('dotenv').config()
+
+const _      = require('lodash'),
+      expect = require('expect'),
+      jsonf  = require('jsonfile'),
+      trillo = require('../dist/trillo'),
+      Trello = require('../dist/trello').Trello,
+      trello = new Trello({
+        key:   process.env.TRELLO_API_KEY,
+        token: process.env.TRELLO_API_TOKEN
+      })
+
+module.exports._      = _
+module.exports.expect = expect
+module.exports.jsonf  = jsonf
+module.exports.trillo = trillo
+module.exports.trello = trello

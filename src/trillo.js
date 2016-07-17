f12cf13615fe7373430a153be0d935d73fbe858d
David Hodges <hodges.david@gmail.com>
2016-07-17 02:42:49 +0100

    update or insert cards to the db

--- src/trillo.js
+++ src/trillo.js
@@ -37,23 +37,34 @@ const parseCardMembers = (members) => {
   }))
 }
 
-const run = function() {
-  const doingListId = process.env.TRELLO_DOING_LIST_ID
+const doingListId = process.env.TRELLO_DOING_LIST_ID
 
+const run = function() {
   trello.getCardsOnList(doingListId).then((cards) => {
     _.map(cards, (card) => {
       trello.getCardActions(card.id, {filter:'updateCard'}).then((actions) => {
         trello.getCardLabels(card.id).then((labels) => {
           trello.getCardMembers(card.id).then((members) => {
-            jsonfile.writeFileSync(__dirname+`/${card.id}.json`, {
-              card: {
-                id:      card.id,
-                name:    card.name,
-                labels:  parseCardLabels(labels),
-                members: parseCardMembers(members),
-                actions: parseCardActions(actions)
+            const card_data = {
+              id:      card.id,
+              name:    card.name,
+              labels:  parseCardLabels(labels),
+              members: parseCardMembers(members),
+              actions: parseCardActions(actions)
+            }
+
+            db_query("SELECT * FROM cards WHERE id = $1::varchar(24)", [card.id], (err, rows) => {
+              if (err) throw err
+
+              if (rows.length > 0) {
+                db_query("UPDATE cards SET name = $1::varchar(255), data = $2::json WHERE id = $3::varchar(24)",
+                  [card.name, card_data, card.id], (err, rows) => {if (err) throw err})
               }
-            }, {spaces:2})
+              else {
+                db_query("INSERT INTO cards (id, name, data) VALUES ($1::varchar(24), $2::varchar(255), $3::json)",
+                  [card.id, card.name, card_data], (err, rows) => {if (err) throw err})
+              }
+            })
           })
         })
       })

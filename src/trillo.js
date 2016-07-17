8268a0303a529d4a273c3e50cfbaab355e2e2f36
David Hodges <hodges.david@gmail.com>
2016-07-17 12:38:56 +0100

    extract updateDb()

--- src/trillo.js
+++ src/trillo.js
@@ -41,6 +41,18 @@ const parseCardDescription = (description) => {
   return description._value
 }
 
+const updateDb = (card) => {
+  db_query("SELECT * FROM cards WHERE id = $1::varchar(24)", [card.id], (err, rows) => {
+    if (err) throw err
+
+    const sql = (rows.length > 0)
+      ? "UPDATE cards SET name = $2::varchar(255), data = $3::json WHERE id = $1::varchar(24)"
+      : "INSERT INTO cards (id, name, data) VALUES ($1::varchar(24), $2::varchar(255), $3::json)"
+
+    db_query(sql, [card.id, card.name, card], (err, rows) => {if (err) throw err})
+  })
+}
+
 const doingListId = process.env.TRELLO_DOING_LIST_ID
 
 const run = function() {
@@ -50,26 +62,13 @@ const run = function() {
         trello.getCardLabels(card.id).then((labels) => {
           trello.getCardMembers(card.id).then((members) => {
             trello.getCardDescription(card.id).then((description) => {
-              const card_data = {
+              updateDb({
                 id:      card.id,
                 name:    card.name,
                 labels:  parseCardLabels(labels),
                 members: parseCardMembers(members),
                 actions: parseCardActions(actions),
                 description: parseCardDescription(description)
-              }
-
-              db_query("SELECT * FROM cards WHERE id = $1::varchar(24)", [card.id], (err, rows) => {
-                if (err) throw err
-
-                if (rows.length > 0) {
-                  db_query("UPDATE cards SET name = $1::varchar(255), data = $2::json WHERE id = $3::varchar(24)",
-                    [card.name, card_data, card.id], (err, rows) => {if (err) throw err})
-                }
-                else {
-                  db_query("INSERT INTO cards (id, name, data) VALUES ($1::varchar(24), $2::varchar(255), $3::json)",
-                    [card.id, card.name, card_data], (err, rows) => {if (err) throw err})
-                }
               })
             })
           })

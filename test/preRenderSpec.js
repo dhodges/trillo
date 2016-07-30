5709ff44026d6631c943dafd44d9480d667bf23c
david hodges <david.hodges@lonelyplanet.com>
2016-07-30 13:11:24 +1000

    update test fixture

--- test/preRenderSpec.js
+++ test/preRenderSpec.js
@@ -10,13 +10,13 @@ try {
     const meta  = cards.meta
 
     it ('gets the cards', () => {
-      expect(cards.cards.length).to.equal(5)
+      expect(cards.cards.length).to.equal(47)
     })
 
     describe ('meta', () => {
       it ('exists', () => expect(meta).to.not.be.undefined)
-      it ('contains chart_from_date', () => expect(meta.chart_from_date).to.not.be.undefined)
-      it ('contains chart_to_date',   () => expect(meta.chart_to_date).to.not.be.undefined)
+      it ('contains chart_from_date', () => expect(meta.chart_from_date).to.equal('2016-05-31T17:19:22.405Z'))
+      it ('contains chart_to_date',   () => expect(meta.chart_to_date).to.equal('2016-06-29T17:19:22.405Z'))
     })
   })
 }
--- test/utilsSpec.js
+++ test/utilsSpec.js
@@ -10,15 +10,15 @@ try {
 
     it ('for cards deployed to prod', () => {
       const deployedCards = utils.deployedCards(chart_data.cards)
-      expect(deployedCards.length).to.equal(4)
+      expect(deployedCards.length).to.equal(42)
     })
 
     it ('for date deployed', () => {
-      expect(utils.dateDeployed(chart_data.cards[3])).to.equal('2016-06-17T00:23:23.977Z')
+      expect(utils.dateDeployed(chart_data.cards[3])).to.equal('2016-06-23T09:46:07.210Z')
     })
 
     it ('for date started Doing', () => {
-      expect(utils.dateStartedDoing(chart_data.cards[0])).to.equal('2016-06-10T08:48:52.445Z')
+      expect(utils.dateStartedDoing(chart_data.cards[0])).to.equal('2016-06-17T01:16:32.405Z')
     })
 
     it ('for determining previous month', () => {
@@ -29,5 +29,6 @@ try {
 }
 catch(err) {
   console.error(`error: ${err.message}`)
-  throw err
+  throw err.message
+  // throw err
 }
